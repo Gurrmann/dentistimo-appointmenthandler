c2b0247f5f08ae712eef6fae11b5b1fac4f05d0e
Emil Gustafsson <emgusta@student.chalmers.se>
2020-12-01 22:33:04 +0100

    Add method for saving appointments

--- DataRetriever.js
+++ DataRetriever.js
@@ -3,7 +3,8 @@ var client = mqtt.connect('mqtt://localhost:1883')
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/dentistimoDB')
 var db = mongoose.connection;
-var Appointment = require('./models/appointments')
+var Appointment = require('./models/appointments');
+const appointment = require('./models/appointment');
 
 client.on('connect', function () {
   client.subscribe('validBookingRequest')
@@ -17,6 +18,20 @@ client.on('message', function (topic, message) {
 var checkBooking = () = => {
 
   notifyUser(true,'')
+}
+
+var saveAppointment = (data) => {
+  var appointmentData = {
+    dentistry: data.dentistid,
+    timeSlot: data.time,
+    userId: data.userid,
+  }
+  var appointment = new Appointment(appointmentData)
+  appointment.save(function(err, result){
+    if(err){
+      console.log(err)
+    }
+  })
 }
 
 var notifyUser = (bookingExist, userid) => {
