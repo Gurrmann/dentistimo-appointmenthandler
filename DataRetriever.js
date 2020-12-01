773eeb0008e96322c6c3db06017f4c0bc2b589c3
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-12-01 20:26:10 +0100

    Properly connected to jsvalidator

--- DataRetriever.js
+++ DataRetriever.js
@@ -1,2 +1,11 @@
 var mqtt = require('mqtt')
-var client = mqtt.connect('ws://test.mosquitto.org:8080')
+var client = mqtt.connect('mqtt://test.mosquitto.org')
+
+client.on('connect', function () {
+  client.subscribe('validBookingRequest')
+})
+
+client.on('message', function (topic, message) {
+  message = JSON.parse(message)
+
+})
