965f2b081a2a30797a81f210ea27287423ddf42a
Gustav Skallberg <gustav.skallberg@gmail.com>
2020-12-01 20:48:50 +0100

    Established connection to database

--- DataRetriever.js
+++ DataRetriever.js
@@ -1,5 +1,9 @@
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://test.mosquitto.org')
+var client = mqtt.connect('mqtt://localhost:1883')
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/dentistimoDB')
+var db = mongoose.connection;
+var Appointment = require('./models/appointments')
 
 client.on('connect', function () {
   client.subscribe('validBookingRequest')
